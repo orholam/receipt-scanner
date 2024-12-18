746472b278da3de30be0bd3a906092ae134a1727
orholam <josiaha@uchicago.edu>
2024-12-18 16:34:57 -0600

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import dotenv from 'dotenv';
+dotenv.config();
 
 createRoot(document.getElementById("root")!).render(<App />);
