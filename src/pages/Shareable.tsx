fd93d440c3c5883ac0897ba1c10322fbdf86d4a6
orholam <josiaha@uchicago.edu>
2025-01-12 01:23:38 -0600

    claim works!

--- src/pages/Shareable.tsx
+++ src/pages/Shareable.tsx
@@ -7,36 +7,59 @@ const Shareable = () => {
   const [nickname, setNickname] = useState<string | null>(null);
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
   const [items, setItems] = useState<any[]>([]);
+  const [claimedItems, setClaimedItems] = useState({});
+  const [unclaimedItems, setUnclaimedItems] = useState<any[]>([]);
   const [transactionName, setTransactionName] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const supabase = useSupabase();
   const { id } = useParams();
 
+  // Fetch transaction
+  const fetchTransaction = async () => {
+    const { data, error } = await supabase.from('transaction').select('*').eq('id', id).single();
+    if (error) {
+      console.error('Error fetching transaction:', error);
+      setError('Error fetching transaction');
+    } else {
+      setTransactionName(data.restaurant);
+    }
+  };
+
+  // Divide items into claimed and unclaimed
+  const divideItems = (items: any[]) => {
+    const claimedItemsRaw = items.filter((item) => item.owner_nickname);
+    const unclaimedItems = items.filter((item) => !item.owner_nickname);
+    const claimedItems = {};
+
+    claimedItemsRaw.forEach((item) => {
+      if (!claimedItems[item.owner_nickname]) {
+        claimedItems[item.owner_nickname] = [];
+      }
+      claimedItems[item.owner_nickname].push(item);
+    });
+
+    setClaimedItems(claimedItems);
+    setUnclaimedItems(unclaimedItems);
+    console.log("items", items);
+    console.log("claimed items", claimedItems);
+    console.log("unclaimed items", unclaimedItems);
+  };
+
+  // Fetch items
+  const fetchItems = async () => {
+    const { data, error } = await supabase.from('item').select('*').eq('transaction_id', id);
+    if (error) {
+      console.error('Error fetching items:', error);
+      setError('Error fetching items');
+    } else {
+      setItems(data);
+      divideItems(data);
+    }
+  };
+
   useEffect(() => {
-    // Fetch transaction
-    const fetchTransaction = async () => {
-      const { data, error } = await supabase.from('transaction').select('*').eq('id', id).single();
-      if (error) {
-        console.error('Error fetching transaction:', error);
-        setError('Error fetching transaction');
-      } else {
-        setTransactionName(data.restaurant);
-      }
-    };
-
-    // Fetch items
-    const fetchItems = async () => {
-      const { data, error } = await supabase.from('item').select('*').eq('transaction_id', id);
-      if (error) {
-        console.error('Error fetching transaction:', error);
-        setError('Error fetching transaction');
-      } else {
-        setItems(data);
-      }
-    };
-
-    fetchItems();
     fetchTransaction();
+    fetchItems();
   }, [id, supabase]);
 
   const toggleItemSelection = (itemName: string) => {
@@ -50,6 +73,21 @@ const Shareable = () => {
   if (error) {
     return <div>{error}</div>;
   }
+
+  const updateSelectedItems = async () => {
+    console.log(selectedItems);
+
+    setUnclaimedItems(unclaimedItems.filter((item) => !selectedItems.includes(item.id)));
+    // Update database
+    const { data, error } = await supabase.from('item').update({ owner_nickname: nickname }).eq('transaction_id', id).in('id', selectedItems);
+    if (error) {
+      console.error('Error updating items:', error);
+      setError('Error updating items');
+    } else {
+      console.log('Items updated successfully:', data);
+      fetchItems();
+    }
+  };
 
   return (
     <div className="flex flex-col flex-grow items-center justify-center bg-gradient-to-b from-indigo-100 to-white pmd:p-8 mx-4 my-4 rounded-lg mb-10">
@@ -89,14 +127,24 @@ const Shareable = () => {
           {isNicknameSet && (
             <>
               <h1 className="text-3xl font-semibold text-gray-800 mb-6 text-center">{transactionName}</h1>
-              <p className="text-gray-800 mb-6 text-center">What items were yours {nickname}?</p>
               <div className={`space-y-4 w-full transition-all duration-300 ${selectedItems.length > 0 ? 'mb-6' : ''}`}>
-                {items.map((item) => (
+                {Object.keys(claimedItems).map((item) => (
+                  <>
+                    <div className="flex flex-col w-full px-4 py-2 rounded-lg bg-orange-100 text-gray-800 border-t-4 border-gray-300">
+                      <p className="text-right">{item}</p>
+                      {claimedItems[item].map((item) => (
+                        <p>{item.item_name} - ${item.cost}</p>
+                      ))}
+                    </div>
+                  </>
+                ))}
+                <p className="text-gray-800 mb-6 text-center">What items were yours {nickname}?</p>
+                {unclaimedItems.map((item) => (
                   <div
-                    key={`${item.id}-${item.item_name}`}
-                    onClick={() => toggleItemSelection(`${item.id}-${item.item_name}`)}
+                    key={`${item.id}`}
+                    onClick={() => toggleItemSelection(`${item.id}`)}
                     className={`cursor-pointer w-full px-4 py-2 rounded-full border text-center ${
-                      selectedItems.includes(`${item.id}-${item.item_name}`)
+                      selectedItems.includes(`${item.id}`)
                         ? 'bg-blue-400 text-white'
                         : 'bg-white text-gray-800 border border-grey-300'
                     } transition-colors duration-200`}
@@ -108,6 +156,7 @@ const Shareable = () => {
               {selectedItems.length > 0 && (
                 <button
                   className="w-full bg-blue-400 text-white py-2 rounded-lg hover:bg-blue-500 transition-opacity duration-300 opacity-0 animate-fade-float-in"
+                  onClick={updateSelectedItems}
                 >
                   Claim
                 </button>
