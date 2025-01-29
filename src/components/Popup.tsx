4213d82905ff915f7c046791cd7e6e4c13d3317b
orholam <josiaha@uchicago.edu>
2025-01-28 18:10:31 -0600

    wip

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -4,7 +4,7 @@ import { X } from "lucide-react";
 
 const Popup = ( { onClose, onTrySample }: { onClose: () => void, onTrySample: () => void } ) => {
     return (
-        <div className="flex gap-2 items-center justify-between bg-gray-100 rounded-xl p-4 z-50">
+        <div className="flex gap-1 items-center justify-between bg-gray-100 rounded-xl p-4 z-50">
             <div className="flex items-center">
                 <img src={undrawReceipt} alt="Sample Receipt" className="w-12 h-12 mr-4" />
                 <div>
@@ -16,7 +16,7 @@ const Popup = ( { onClose, onTrySample }: { onClose: () => void, onTrySample: ()
                 <Button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-300" onClick={onTrySample}>
                     Try it out
                 </Button>
-                <Button className="px-4 py-2 text-black bg-gray-100 rounded-lg hover:bg-gray-300 transition duration-300" onClick={onClose}>
+                <Button className="py-2 text-black bg-gray-100 rounded-lg hover:bg-gray-300 transition duration-300" onClick={onClose}>
                     <X className="w-4 h-4" />
                 </Button>
             </div>
