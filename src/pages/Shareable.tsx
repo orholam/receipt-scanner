048e5099df4ebe60d3f26e8218e7f6aa0a0c2ac9
kientea24 <kientea24@gmail.com>
2025-04-24 18:22:21 -0400

    Update Shareable.tsx

--- src/pages/Shareable.tsx
+++ src/pages/Shareable.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSupabase } from '@/SupabaseContext';
-import { X } from 'lucide-react';
+import { X, ArrowLeft } from 'lucide-react'; // Import the ArrowLeft icon
 import Header from '@/components/Header';
 import venmoLogo from '@/assets/venmo.svg';
 
@@ -19,7 +19,7 @@ const Shareable = () => {
   const [paymentMethod, setPaymentMethod] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
   const [showHelp, setShowHelp] = useState<boolean>(false); // Add state for help text
-  const helpText = "Claim your items, see your total, and pay back what you owe!";
+  const helpText = "Select to claim your items, see your total, and pay back what you owe!";
   const supabase = useSupabase();
   const { id } = useParams();
 
@@ -68,32 +68,24 @@ const Shareable = () => {
     const claimedItems = {};
 
     claimedItemsRaw.forEach((item) => {
-      if (!claimedItems[item.owner_nickname]) {
-        claimedItems[item.owner_nickname] = [];
+      const ownerNickname = item.owner_nickname.toLowerCase(); // Normalize to lowercase
+      if (!claimedItems[ownerNickname]) {
+        claimedItems[ownerNickname] = [];
       }
-      claimedItems[item.owner_nickname].push(item);
+      claimedItems[ownerNickname].push(item);
     });
 
     const individualTotals = {};
     Object.keys(claimedItems).forEach((nickname) => {
-      console.log("nickname", nickname);
-      console.log("claimedItems[nickname]", claimedItems[nickname]);
-      const individualPreTax = claimedItems[nickname].reduce((acc, item) => acc + item.cost, 0);
-      console.log("individualPreTax", individualPreTax);
-      console.log("total", transaction.total);
-      console.log("tax", transaction.tax);
-      console.log("tip", transaction.tip);
+      const normalizedNickname = nickname.toLowerCase(); // Ensure consistent comparison
+      const individualPreTax = claimedItems[normalizedNickname].reduce((acc, item) => acc + item.cost, 0);
       const { individualTotal, individualTax, individualTip } = calcTaxTipTotalShare(transaction.total, transaction.tax, transaction.tip, individualPreTax);
-      individualTotals[nickname] = { individualTotal, individualTax, individualTip };
+      individualTotals[normalizedNickname] = { individualTotal, individualTax, individualTip };
     });
-    console.log("individualTotals", individualTotals);
 
     setClaimedItems(claimedItems);
     setUnclaimedItems(unclaimedItems);
     setIndividualTotals(individualTotals);
-    console.log("items", items);
-    console.log("claimed items", claimedItems);
-    console.log("unclaimed items", unclaimedItems);
   };
 
   // Fetch items
@@ -167,12 +159,14 @@ const Shareable = () => {
     }
 
     // Filter out items that are already claimed by others
-    const unclaimedItemsToClaim = latestItems.filter((item) => !item.owner_nickname);
+    const unclaimedItemsToClaim = latestItems.filter(
+      (item) => !item.owner_nickname || item.owner_nickname.toLowerCase() === nickname?.toLowerCase()
+    );
 
     // Update database for unclaimed items
     const { data, error } = await supabase
       .from('item')
-      .update({ owner_nickname: nickname })
+      .update({ owner_nickname: nickname?.toLowerCase() }) // Normalize nickname to lowercase
       .eq('transaction_id', id)
       .in('id', unclaimedItemsToClaim.map((item) => item.id));
 
@@ -193,7 +187,7 @@ const Shareable = () => {
       .from('item')
       .update({ owner_nickname: null })
       .eq('transaction_id', id)
-      .eq('owner_nickname', nickname);
+      .eq('owner_nickname', nickname?.toLowerCase()); // Normalize nickname to lowercase
     if (error) {
       console.error('Error unclaiming items:', error);
       setError('Error unclaiming items');
@@ -222,7 +216,7 @@ const Shareable = () => {
                 type="text"
                 placeholder="Type here..."
                 className="w-full px-4 py-2 rounded-full border text-center"
-                onChange={(e) => setNickname(e.target.value)}
+                onChange={(e) => setNickname(e.target.value.toLowerCase())} // Normalize input to lowercase
                 onKeyDown={(e) => {
                   if (e.key === 'Enter' && nickname && nickname.trim() !== '') {
                     setIsNicknameSet(true);
@@ -248,41 +242,47 @@ const Shareable = () => {
           )}
         </div>
         <div className={`transition-opacity duration-700 ${isNicknameSet ? 'opacity-100' : 'opacity-0'}`}>
-          {isNicknameSet && paymentUsername && paymentMethod && (
-            <div className="flex justify-center mb-6 relative">
-              {paymentMethod === 'Venmo' ? (
-                <a
-                  href={`https://account.venmo.com/u/${paymentUsername.replace('@', '')}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="px-4 py-2 inline-flex items-center gap-2 text-center rounded-full bg-gradient-to-r from-blue-50 to-blue-100 text-gray-800 shadow-lg hover:underline"
-                >
-                  <b>{paymentMethod}</b>
-                  {paymentUsername}
-                </a>
-              ) : (
-                <p className="px-4 py-2 inline-flex items-center gap-2 text-center rounded-full bg-gradient-to-r from-blue-50 to-blue-100 text-gray-800 shadow-lg">
-                  <b>{paymentMethod}</b>
-                  {paymentUsername}
-                </p>
-              )}
-              <button
-                type="button"
-                onClick={() => setShowHelp(!showHelp)}
-                className="absolute top-[-10px] right-[-20px] text-black border border-black rounded-full w-8 h-8 flex items-center justify-center"
-                aria-label="Help"
-              >
-                ?
-              </button>
-              {showHelp && (
-                <div className="absolute top-10 right-[-50px] bg-white border border-gray-300 shadow-lg rounded-md p-4 w-64 z-10">
-                  <p className="text-sm text-gray-700">{helpText}</p>
-                </div>
-              )}
-            </div>
-          )}
           {isNicknameSet && (
             <>
+              <div className="flex items-center mb-4">
+                <ArrowLeft
+                  className="h-6 w-6 cursor-pointer text-gray-800 hover:text-gray-600" // Reduced size for smaller button
+                  onClick={() => setIsNicknameSet(false)} // Go back to the nickname input page
+                />
+              </div>
+              <div className="flex justify-center mb-6 relative">
+                {paymentUsername && paymentMethod ? (
+                  paymentMethod === 'Venmo' ? (
+                    <a
+                      href={`https://account.venmo.com/u/${paymentUsername.replace('@', '')}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="px-4 py-2 inline-flex items-center gap-2 text-center rounded-full bg-gradient-to-r from-blue-50 to-blue-100 text-gray-800 shadow-lg hover:underline"
+                    >
+                      <b>{paymentMethod}</b>
+                      {paymentUsername}
+                    </a>
+                  ) : (
+                    <p className="px-4 py-2 inline-flex items-center gap-2 text-center rounded-full bg-gradient-to-r from-blue-50 to-blue-100 text-gray-800 shadow-lg">
+                      <b>{paymentMethod}</b>
+                      {paymentUsername}
+                    </p>
+                  )
+                ) : null}
+                <button
+                  type="button"
+                  onClick={() => setShowHelp(!showHelp)}
+                  className="absolute top-[-45px] right-[-20px] text-black border border-black rounded-full w-8 h-8 flex items-center justify-center" // Adjusted top position further upward
+                  aria-label="Help"
+                >
+                  ?
+                </button>
+                {showHelp && (
+                  <div className="absolute top-10 right-[-50px] bg-white border border-gray-300 shadow-lg rounded-md p-4 w-64 z-10">
+                    <p className="text-sm text-gray-700">{helpText}</p>
+                  </div>
+                )}
+              </div>
               <h1 className="text-3xl font-semibold text-gray-800 mb-6 text-center">{transactionName}</h1>
               <div className={`space-y-4 w-full transition-all duration-300 ${selectedItems.length > 0 ? 'mb-6' : ''}`}>
                 {Object.keys(claimedItems).map((item) => (
